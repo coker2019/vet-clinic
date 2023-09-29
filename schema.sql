2928dd1f47f4326beabf5c9a573ac082cb14bc45
coker2019 <olabisi2k11@gmail.com>
2023-09-29 12:53:05 +0100

    Modify animals table.

--- schema.sql
+++ schema.sql
@@ -12,3 +12,24 @@ CREATE TABLE animals(
 -- Modify the animals' table to add species column.
 ALTER TABLE animals
 ADD COLUMN species VARCHAR(100);
+
+-- Create the owners table
+CREATE TABLE owners (
+  id SERIAL PRIMARY KEY,
+  full_name VARCHAR(100),
+  age INTEGER
+);
+
+-- Create the species table
+CREATE TABLE species (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100)
+);
+
+-- Modify the animals table
+ALTER TABLE animals
+DROP COLUMN species;
+
+ALTER TABLE animals
+ADD COLUMN species_id INTEGER REFERENCES species(id),
+ADD COLUMN owner_id INTEGER REFERENCES owners(id);
